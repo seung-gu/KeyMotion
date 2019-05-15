8d43a19d71b0f6c1b50f13cc1a5239b47eb44535
seung-gu <seunggu.kang.kr@gmail.com>
2019-05-16 08:47:07 +0900

    KeyMotion version 1b - Left,Right,Down movement message

---
+++ CMakeFiles/KeyMotion_v1b.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/MyListener.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/MyListener.cpp.o"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/bluetooth/Bluetooth.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/bluetooth/Bluetooth.cpp.o"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/display/sh1106/Adafruit_GFX.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/display/sh1106/Adafruit_GFX.cpp.o"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/display/sh1106/ArduiPi_OLED.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/display/sh1106/ArduiPi_OLED.cpp.o"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/main.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/main.cpp.o"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/sound/BeepSound.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/KeyMotion_v1b.dir/sound/BeepSound.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/include"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/include/royaleCAPI"
+  "cpp/KeyMotion/KeyMotion_v1b/../../../inc"
+  "cpp/KeyMotion/KeyMotion_v1b/display/sh1106"
+  "cpp/KeyMotion/KeyMotion_v1b/bluetooth"
+  "cpp/KeyMotion/KeyMotion_v1b/sound"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/LibsKeyMotion_v1b.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/LibsKeyMotion_v1b.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/main.cpp" "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/samples/cpp/KeyMotion/KeyMotion_v1b/CMakeFiles/LibsKeyMotion_v1b.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/include"
+  "/home/pi/Downloads/OpenLicht/libroyale-3.17.0.56-LINUX-arm-32Bit/include/royaleCAPI"
+  "cpp/KeyMotion/KeyMotion_v1b/../../../inc"
+  "cpp/KeyMotion/KeyMotion_v1b/display/sh1106"
+  "cpp/KeyMotion/KeyMotion_v1b/bluetooth"
+  "cpp/KeyMotion/KeyMotion_v1b/sound"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
